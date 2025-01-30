1ca383a8b85972418189c931c6e04b2c81851355
Stefan Urbanek <stefan@agentfarms.net>
2025-01-30 21:50:02 +0100

    Use icons in toolbar
    - added select and connect icons
    - modified toolbar to use IconButton

--- gui/tool_bar.gd
+++ gui/tool_bar.gd
@@ -1,21 +1,23 @@
 class_name ToolBar extends PanelContainer
 
-@onready var selection_tool_button: Button = $HBoxContainer/SelectionToolButton
-@onready var connection_tool_button: Button = $HBoxContainer/ConnectToolButton
+@onready var selection_tool_button: IconButton = %SelectionToolButton
+@onready var connection_tool_button: IconButton = %ConnectToolButton
 
 func _ready():
 	Global.tool_changed.connect(_on_tool_changed)
-	print("HERE: ", selection_tool_button)
+	Global.change_tool(Global.selection_tool)
 
 func _on_tool_changed(tool: CanvasTool):
 	if tool is SelectionTool:
-		selection_tool_button.button_pressed = true
-		connection_tool_button.button_pressed = false
-	elif tool is SelectionTool:
-		selection_tool_button.button_pressed = false
-		connection_tool_button.button_pressed = true
+		selection_tool_button.set_pressed_no_signal(true)
+		connection_tool_button.set_pressed_no_signal(false)
+	elif tool is ConnectTool:
+		selection_tool_button.set_pressed_no_signal(false)
+		connection_tool_button.set_pressed_no_signal(true)
 	else:
 		push_error("Unknown tool: ", tool)
+	selection_tool_button.update_shader()
+	connection_tool_button.update_shader()
 
 func _on_selection_tool_button_pressed():
 	Global.change_tool(Global.selection_tool)
