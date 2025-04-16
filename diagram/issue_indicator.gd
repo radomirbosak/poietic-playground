ac782bd249e35ca552503a301ae59a2eac325ef0
Stefan Urbanek <stefan@agentfarms.net>
2025-04-16 11:53:09 +0200

    Fixed hit box for issue indicator
    it was of the original icon size, ignoring icon scale

--- diagram/issue_indicator.gd
+++ diagram/issue_indicator.gd
@@ -1,21 +1,22 @@
 class_name IssueIndicator extends Node2D
 
-var width: float = 48
+var size: float = 48
 var icon: Sprite2D
+var icon_scale: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	icon = Sprite2D.new()
 	icon.texture = load("res://resources/icons/error.png")
-	var size = icon.texture.get_size()
-	var scale = width / size.x
-	icon.scale = Vector2(scale, scale)
+	var icon_size = icon.texture.get_size()
+	icon_scale = size / icon_size.x
+	icon.scale = Vector2(icon_scale, icon_scale)
 	icon.position.y -= 3
 	icon.z_index = DiagramCanvas.issues_indicator_z_index + 1
 	add_child(icon)	
 
 func get_rect() -> Rect2:
-	return icon.get_rect()
+	return Rect2(-size / 2, -size / 2, size, size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
